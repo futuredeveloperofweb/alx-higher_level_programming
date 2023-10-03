e388f62542f3865ddd9a5c7c3a2978ab61945cb0
othmane <othmane.alaouimekkaoui@gmail.com>
2023-10-03 15:32:51 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i + 1, 10):
-        print('{:02d}'.format(i * 10 + j), end=' ')
-    print()
+        print('{:02d}'.format(i * 10 + j), end=', ')
+print('\b\b', end=' ')
+print()
