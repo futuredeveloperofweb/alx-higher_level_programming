fabd51e1edb6ac19cd3783c790967e2c9fa9e582
othmane <othmane.alaouimekkaoui@gmail.com>
2023-10-11 15:40:46 -0700

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if not my_list:
         return 0
-    s = 0
-    d = 0
-    for t in range(len(my_list)):
-        s += my_list[t][0] * my_list[t][1]
-        d += my_list[t][1]
-    return s/d
+    else:
+        return (sum(a * b for a, b in my_list) / sum(b for a, b in my_list))
