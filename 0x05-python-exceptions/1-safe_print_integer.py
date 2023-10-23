fdac7dee591007020693370959654ef5de37f6aa
othmane <othmane.alaouimekkaoui@gmail.com>
2023-10-23 11:55:27 -0700

    task 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    int_val = 0
     try:
-        int_val == int(value)
         print('{:d}'.format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
