bef5be9950b4c4da85f29602b8b3ffa7e7b90dfa
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-01 08:14:12 -0700

    task6

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,61 +1,87 @@
 #!/usr/bin/python3
-"""Module for matrix_mul function"""
+"""Module for matrix_mul method"""
 
 
 def matrix_mul(m_a, m_b):
-    """a function that multiplies 2 matrices
+    """Multiplication of two matrices
+
     Args:
         m_a: the first matrix
         m_b: the second matrix
+
     Returns:
-        the multiplication of m_a and m_b
+        matrix: the product
+
     Raises:
+        TypeError: If m_a or m_b are not lists.
+        TypeError: If m_a or m_b are not lists of lists.
+        ValueError: If m_a or m_b are empty lists/matrices.
+        TypeError: If m_a or m_b contain a non int/float.
+        TypeError: If m_a or m_b are not rectangular.
+        ValueError: If m_a or m_b can't be multiplied.
     """
+
     if not isinstance(m_a, list):
-        raise TypeError('m_a must be a list')
+        raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
-        raise TypeError('m_b must be a list')
-    for raw in m_a:
-        if not isinstance(raw, list):
-            raise TypeError('m_a must be a list of lists')
-    for raw in m_b:
-        if not isinstance(raw, list):
-            raise TypeError('m_b must be a list of lists')
-    if m_a == [] or m_a == [[]]:
-        raise TypeError('m_a can\'t be empty')
-    if m_b == [] or m_b == [[]]:
-        raise TypeError('m_b can\'t be empty')
-    for raw in m_a:
-        for e in raw:
-            if not isinstance(e, (int, float)):
-                raise TypeError('m_a should contain only integers or floats')
-    for raw in m_b:
-        for e in raw:
-            if not isinstance(e, (int, float)):
-                raise TypeError('m_b should contain only integers or floats')
+        raise TypeError("m_b must be a list")
+    m_a_empty = False
+    m_b_empty = False
+    m_a_notrect = False
+    m_b_notrect = False
+    m_a_notnum = False
+    m_b_notnum = False
+    for row in m_a:
+        if not isinstance(row, list):
+            raise TypeError("m_a must be a list of lists")
+        if len(row) != len(m_a[0]):
+            m_a_notrect = True
+        for num in row:
+            if not isinstance(num, (int, float)):
+                m_a_notnum = True
 
-    l_a = len(m_a[0])
-    l_b = len(m_b[0])
+    for row in m_b:
+        if not isinstance(row, list):
+            raise TypeError("m_b must be a list of lists")
+        if len(row) != len(m_b[0]):
+            m_b_notrect = True
+        for num in row:
+            if not isinstance(num, (int, float)):
+                m_b_notnum = True
 
-    for raw in m_a[1:]:
-        if len(raw) != l_a:
-            raise TypeError('each row of m_a must be of the same size')
-    for raw in m_b[1:]:
-        if len(raw) != l_b:
-            raise TypeError('each row of m_b must be of the same size')
+    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
+        raise ValueError("m_a can't be empty")
+
+    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
+        raise ValueError("m_b can't be empty")
+
+    if m_a_notnum:
+        raise TypeError("m_a should contain only integers or floats")
+
+    if m_b_notnum:
+        raise TypeError("m_b should contain only integers or floats")
+
+    if m_a_notrect:
+        raise TypeError("each row of m_a must should be of the same size")
+
+    if m_b_notrect:
+        raise TypeError("each row of m_b must should be of the same size")
+
     if len(m_a[0]) != len(m_b):
-        raise ValueError('m_a and m_b can\'t be multiplied')
+        raise ValueError("m_a and m_b can't be multiplied")
 
-    m_c = [[] for i in range(len(m_a))]
+    res = [[] for i in range(len(m_a))]
+
     for i in range(len(m_a)):
         for j in range(len(m_b[0])):
             c = 0
             for k in range(len(m_b)):
                 c += m_a[i][k] * m_b[k][j]
-            m_c[i].append(c)
-    return (m_c)
+            res[i].append(c)
+
+    return res
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import doctest
-    doctest.testfile('tests/100-matrix_mul.txt')
+    doctest.testfile("tests/100-matrix_mul.txt")
