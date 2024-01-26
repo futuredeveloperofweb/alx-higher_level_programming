defc62c65b9b3aeb89c1d02e05c2a66b2731bd37
othmane <othmane.alaouimekkaoui@gmail.com>
2024-01-26 13:14:24 -0800

    t1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
+import sys
 
 
-
+url = sys.argv[1]
+with urllib.request.urlopen(url) as resp:
+    html = resp.read()
+print(resp.getheader('X-Request-Id'))
