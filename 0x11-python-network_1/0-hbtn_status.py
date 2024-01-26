20bafc9531b2bed6026d1def37bf14393790aafe
othmane <othmane.alaouimekkaoui@gmail.com>
2024-01-26 13:05:38 -0800

    t1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,3 +4,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
     html = resp.read()
+print('Body response:')
+print('\t- type:', type(html))
+print('\t- content:', html)
+print('\t- utf8 content:', html.decode('utf-8'))
