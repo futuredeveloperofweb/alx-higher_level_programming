7dc526a444c3810bfd9fba9c04eec9cbf08e6770
othmane <othmane.alaouimekkaoui@gmail.com>
2024-01-26 17:40:53 -0800

    ask 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id'''
+import requests
+import requests.auth
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    token = sys.argv[2]
+    auth = requests.auth.HTTPBasicAuth(username, token)
+    r = requests.get('https://api.github.com/user', auth=auth)
+    print(r.json().get('id'))
