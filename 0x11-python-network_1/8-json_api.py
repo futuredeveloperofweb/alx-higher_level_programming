b90c88da8a4434a345e5995367877713279ba697
othmane <othmane.alaouimekkaoui@gmail.com>
2024-01-26 17:45:50 -0800

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,18 +7,18 @@ import sys
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) == 2:
-        q = sys.argv[1]
+    if len(sys.argv) == 1:
+        q = ''
     else:
-        q = ""
-    l = {'q': q}
-    r = requests.post(url, data=l)
-    
+        q = sys.argv[1]
+    let = {'q': q}
+    r = requests.post(url, data=let)
+
     try:
         resp = r.json()
         if resp == {}:
             print('No result')
         else:
-            print('{[]} {}'.format(resp.get('id'), resp.get('name')))
+            print('[{}] {}'.format(resp.get('id'), resp.get('name')))
     except ValueError:
         print('Not a valid JSON')
