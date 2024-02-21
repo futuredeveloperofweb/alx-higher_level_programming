6d358297165dae82017ded490f727a4fd112ab80
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-21 13:59:45 -0800

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file.
+// Read from file
 
 const filesys = require('fs');
-
 filesys.readFile(process.argv[2], 'utf-8',
-	function (err, data) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(data);
-	});
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-//a script that writes a string to a file.
 
-const fil = require('fil');
+const fs = require('fs');
 
-fil.writeFile(process.argv[2], process.argv[3], 'utf-8',
-	function (err) {
-		if (err) {
-			console.log(err);
-		}
-	});
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
